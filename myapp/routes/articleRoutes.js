6f4bc5b5bcbcf5868c256df09fa39968d76c6f2f
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-30 23:45:45 -0400

    cleaning up

--- myapp/routes/articleRoutes.js
+++ myapp/routes/articleRoutes.js
@@ -4,14 +4,14 @@ const cheerio = require("cheerio");
 
 export default app => {
 // Routes
-// A GET route for scraping the echoJS website
+// A GET route for scraping the newsWeek website
 app.get("/scrape", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.newsweek.com/newsfeed").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
-      // Now, we grab every h2 within an article tag, and do the following:
+      // Now, we grab every h3 within an article tag, and do the following:
       $("article h3").each(function(i, element) {
         // Save an empty result object
         var result = {};
