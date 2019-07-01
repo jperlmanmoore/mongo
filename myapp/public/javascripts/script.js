bdc96ad3c09d71fd312a860d409d2b4bd72a7797
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-01 01:07:31 -0400

    routes

--- myapp/public/javascripts/script.js
+++ myapp/public/javascripts/script.js
@@ -1,13 +1,13 @@
 
     // Grab the articles as a json
-    $.getJSON("/articles", data => {
-      // For each one
-      for (var i = 0; i < data.length; i++) {
-        // Display the apropos information on the page
-        return hbsObject
-      }
-    });
-    
+    // $.getJSON("/Article", data => {
+    //   // For each one
+    //   for (var i = 0; i < data.length; i++) {
+    //     // Display the apropos information on the page
+    //     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    //   }
+    // });
+    console.log(1100);
     // Whenever someone clicks the #scrape button
     $("#scrape").on("click", function (e) {
       e.preventDefault();
@@ -19,9 +19,26 @@
         dataType: 'json',
         success: function (response) {
           console.log(response);
+          const hbsObjArticle = response.results;
+          console.log(hbsObjArticle);
         }
       });
     });
+
+    // Whenever someone clicks the #scrape button
+    $('#saved').on("click", function (e) {
+        e.preventDefault();
+      
+        // Now make an ajax call for the Article
+        $.ajax({
+          method: "GET",
+          url: "/articles",
+          dataType: 'json',
+          success: function (response) {
+            console.log(response);
+          }
+        });
+      });
   
   
 
--- myapp/routes/articleRoutes.js
+++ myapp/routes/articleRoutes.js
@@ -1,31 +1,40 @@
-import { Article, Note } from './models';
-import { get } from "axios";
-import { load } from "cheerio";
+const db = require('./models')
+const axios = require('axios');
+const cheerio = require('cheerio')
+// import { Article, Note } from './models';
+// import { get } from "axios";
+// import { load } from "cheerio";
+
+console.log(900);
 
 modules.exports = app => {
 // Routes
 // A GET route for scraping the newsWeek website
 app.get("/scrape", function(req, res) {
+  console.log(1000)
     // First, we grab the body of the html with axios
-    get("https://www.newsweek.com/newsfeed").then(function(response) {
+    axios.get("https://www.newsweek.com/newsfeed").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = load(response.data);
+      var $ = cheerio.load(response.data);
   
       // Now, we grab every h3 within an article tag, and do the following:
-      $("article h3").each(function(i, element) {
+      $("article").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
         // Add the text and href of every link, and save them as properties of the result object
-        result.title = $(this)
+        result.title = $(element)
           .children("a")
           .text();
-        result.link = $(this)
+        result.link = $(element)
           .children("a")
           .attr("href");
+          result.summary = $(element)
+          .children("a")
+          .attr(".summary");
   
         // Create a new Article using the `result` object built from scraping
-        Article.create(result)
+        db.Article.create(result)
           .then(function(dbArticle) {
             // View the added result in the console
             console.log(dbArticle);
@@ -44,11 +53,11 @@ app.get("/scrape", function(req, res) {
   // Route for getting all Articles from the db
   app.get("/articles", function(req, res) {
     // Grab every document in the Articles collection
-    Article.find({})
+    db.Article.find({})
       .then(function(data) {
         // If we were able to successfully find Articles, send them back to the client
         const hbsResultsObj = {
-          articles: data
+          Article: data
         };
         console.log(hbsResultsObj);
         res.render("index", hbsResultsObj)
@@ -62,7 +71,7 @@ app.get("/scrape", function(req, res) {
   // Route for saving/updating an Article's associated Note
   app.post("/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
-    Note.create(req.body)
+    db.Note.create(req.body)
       .then(function(dbNote) {
         // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
         // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
