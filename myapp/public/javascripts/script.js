f72706a51d3875a1ff1f09fc13af9030dd49a48c
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-06-30 23:35:36 -0400

    working on click handlers

---
+++ myapp/public/javascripts/script.js
@@ -0,0 +1,30 @@
+
+    // Grab the articles as a json
+    $.getJSON("/articles", data => {
+      // For each one
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        return hbsObject
+      }
+    });
+    
+    // Whenever someone clicks the #scrape button
+    $("#scrape").on("click", function (e) {
+      e.preventDefault();
+    
+      // Now make an ajax call for the Article
+      $.ajax({
+        method: "GET",
+        url: "/articles",
+        dataType: 'json',
+        success: function (response) {
+          console.log("response");
+          const hbsResultsObj = response.results
+        }
+      });
+      return hbsResultsObj
+    });
+  
+  
+
+  
