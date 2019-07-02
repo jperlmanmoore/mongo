6fa3f44b4f0bb1f246127d0ab977edcde0d0a873
Jennifer Moore <44844724+jperlmanmoore@users.noreply.github.com>
2019-07-01 20:18:43 -0400

    routes

--- myapp/public/javascripts/script.js
+++ myapp/public/javascripts/script.js
@@ -1,25 +1,59 @@
 $(document).ready(function () {
-//     console.log("document ready");
+     console.log("clicks");
     // Whenever someone clicks the #scrape button
     $('#scrape').on('click', () => {
-     
     console.log("scrape clicked");
-
-        fetch("/api/Article", {method: "GET"}).then(() => window.location.replace("/api/Article"))
+        fetch("/api/search", {method: "GET"}).then(() => window.location.replace("/api/search"))
     });
 
 
-     $('#saved').on('click', ()=> {
+     $('#saved').on('click', function(element) {
          console.log("saved clicked");
 
-         let saved = {
+         let title = $(this).class('title').text();
+         let summary = $(this).class('summary').text();
+         let link = $(this).class('link').text();
+
+         let saved= {
              title,
              summary,
-             link
-         };
+             link,
+             note: null
+         }
 
-         fetch('api/')
-     })
+         fetch('api/articles', {
+            method: 'POST',
+            body: JSON.stringify(saved)
+         }).then((response) => {
+            console.log(response)
+         
+     });
+
+     $('#savedArticles').on('click', function() {
+         console.log("show saved button clicked");
+         fetch("/api/articles", {method: "GET"}).then(() => window.location.replace("/api/search"))
+     });
+
+     $('#saveNote').on('click', function() {
+        let savedNote = $('#textarea1').val().trim();
+
+        let savedNoteObj = {
+            body: {
+                body: savedNote
+            }
+        };
+
+        fetch('/api/Notes', {
+            method: 'POST',
+            body: JSON.stringify(savedNoteObj)
+        }).then(response => {location.reload()});
+     });
+
+    //  delete articles
+
+
+
+
 
     // Whenever someone clicks the #scrape button
     // $(document).on("click", '#saved', function (e) {
@@ -38,3 +72,4 @@ $(document).ready(function () {
   
   
     });
+})
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const mongoose = require('mongoose');
 const db = require('../models')
 
 module.exports = app => {
@@ -8,21 +7,6 @@ module.exports = app => {
         console.log("render index")
       res.render("index", { title: 'Newsweek News Scrapper' });
     });
-
-    app.get("/savedArticles", (req, res) => {
-        console.log("render saved")
-      res.render("savedArticles", { title: 'Newsweek News Scrapper' });
-    });
-
-    app.get("/scrapeArticles", (req, res) => {
-        console.log("render scrape")
-      res.render("scrapeArticles", { title: 'Newsweek News Scrapper' });
-    });
-
-    // app.get("/clearArticles", (req, res) => {
-    //     console.log(1400)
-    //   res.render("scrapeArticles", { title: 'Newsweek News Scrapper' });
-    // });
 
 
 // Routes
@@ -32,6 +16,7 @@ app.get("/search", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.newsweek.com/newsfeed")
     .then(response => {
+      console.log(response.data);
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       const $ = cheerio.load(response.data);
 
@@ -57,7 +42,8 @@ app.get("/search", function(req, res) {
   });
   
   // Route for getting all Articles from the db
-  app.get("/article", function(req, res) {
+  app.get("/api/articles", function(req, res) {
+    console.log("article route")
     // Grab every document in the Articles collection
     db.Article.find({})
       .then(function(data) {
@@ -75,27 +61,33 @@ app.get("/search", function(req, res) {
   });
   
   // Route for saving/updating an Article's associated Note
-  app.post("/article/:id", function(req, res) {
+  app.post("/api/articles/:id", function(req, res) {
     // Create a new note and pass the req.body to the entry
-    db.Note.create(req.body)
-      .then(function(dbNote) {
-        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-        return Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-      })
-      .then(function(dbArticle) {
-        // If we were able to successfully update an Article, send it back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
+    db.insertOne({
+      title: req.body.title,
+      summary: req.body.summary,
+      link: req.body.link}
+      , function(err, result) {
+        if (err) return res.send('Error');
+        res.send('Article added');
       });
-  });
+    });
+      
+
+    // Route for saving/updating an Article's associated Note
+  app.post("/api/note/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.insertOne({
+      title: req.body.title,
+      note: req.body.note,
+    }, function(err, result) {
+        if (err) return res.send('Error');
+        res.send('Article added');
+      });
+    });
+
+    // get article route
 
 });
-
-
 
 }
